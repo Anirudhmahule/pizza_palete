815c314182df83aa7380c1bcdce283f8eb7ae660
Anirudh Mahule <mahuleanirudh@gmail.com>
2023-09-27 12:58:53 +0530

    Added my project

--- src/store.js
+++ src/store.js
@@ -1,9 +1,6 @@
-// Import your reducers first
-import userReducer from '/src/features/user/userSlice.js?t=1695665120522';
-import cartReducer from '/src/features/cart/cartSlice.js?t=1695628988477';
-
-// Then configure the store with the imported reducers
-import { configureStore } from '/node_modules/.vite/deps/@reduxjs_toolkit.js?v=273bbce8';
+import userReducer from './features/user/userSlice';
+import cartReducer from './features/cart/cartSlice';
+import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
   reducer: {
